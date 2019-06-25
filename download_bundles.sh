526ac9dd95eda86a16b9b5d2a12365b2ead779ff
Phelelani Mpangase <pmpangase@gmail.com>
2019-06-25 14:07:28 +0200

    Minor changes

--- download_bundles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# curl --list-only ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37/ > b37_files.txt
-
-while read file
-do
-    lftp -e "pget -n 20 ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37/$file; bye"
-done < b37_files.txt
