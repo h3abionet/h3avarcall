6188349c42c9c05680559b42962a58c1382d3d2a
vagrant <vagrant@vagrant.vm>
2019-06-25 08:16:15 +0000

    Added Singularity recipes and template for downloading bundled data from GATK

---
+++ templates/download_bundles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl --list-only ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37/ > b37_files.txt
+
+while read file
+do
+    lftp -e "pget -n 20 ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37/$file; bye"
+done < b37_files.txt
